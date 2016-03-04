9d9f422df0099b220366b7439d8b75454c4cff98
Alex Schwartz <alexander.schwartz@here.com>
2016-03-04 13:24:26 +0100

    empty balloon milestone: rudimentary biz logic implemented, first tests are in place and green

--- features/step_definitions/poi-stacking.rb
+++ features/step_definitions/poi-stacking.rb
@@ -1,23 +1,40 @@
 Given(/^a POI sequence (.+)$/) do |input_data|
-    @input_data = input_data.split(/,\s*^:/)
+    @input_data = Utils.literal2array(input_data)
 end
 
 When(/^the POIs are stacked$/) do
-    @output = PoiStackingStrategy.new.stack(@input)
+    @output = PoiStackingStrategy.new.stack(@input_data)
 end
 
 Then(/^the POI icons in the stack are equal to (.+)$/) do |expected_output_seq|
-   expected_output_seq_array = expected_output_seq.split(/,\s*/)
-   expect(@output).to eq(expected_output_seq_array)
+   output_literal = Utils.array2literal(@output)
+   
+   # cleanup the expected result ('A,B, C' -> 'A,B,C')
+   expected_output_seq_cleaned = Utils.array2literal(Utils.literal2array(expected_output_seq))
+
+      
+
+   # and now verify
+   expect(output_literal).to eq(expected_output_seq_cleaned)
 end
 
 Then(/^the number of POI icons is less than (\d+)$/) do |arg1|
 end
 
+class Utils
+   def self.literal2array(input_literal)
+       input_literal.split(/,\s*/)
+   end
+
+   def self.array2literal(arr)
+       arr.join(',')
+   end
+end
+
 class PoiStackingStrategy 
-   
+
    def stack(input)
-     [ 'A', 'B' ]
+     input
    end
 
 end
